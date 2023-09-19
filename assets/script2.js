24e12ae0b029d2fa2808e08321ac4e596451ff56
Andreambu23 <andreqro234@gmail.com>
2023-09-19 11:23:49 -0300

    Att. Dld img+xlsx

--- assets/script2.js
+++ assets/script2.js
@@ -1,5 +1,7 @@
+
+
 function meuEscopo() {
-    
+
     const form = document.querySelector('.form')
     const resultado = document.querySelector('.resultado')
 
@@ -10,7 +12,7 @@ function meuEscopo() {
     // };
 
     // let contador = 1
-    function recebeEventoForm(evento){
+    function recebeEventoForm(evento) {
         evento.preventDefault()
         // console.log(`Formulario não foi enviado ${contador}`)
         // contador++
@@ -18,23 +20,43 @@ function meuEscopo() {
         const telefone = form.querySelector('.telefone');
         const pedido = form.querySelector('.pedido');
         const token = document.querySelector(".password").textContent;
-        let containerPassword = document.querySelector("#container-password");
+
         console.log(dados)
-        
+
         dados.push({
             nome: nome.value,
             sobrenome: telefone.value,
             token: token
-            
+
         })
+        const workbook = XLSX.utils.book_new();
+        const worksheet = XLSX.utils.json_to_sheet(dados);
+
+        XLSX.utils.book_append_sheet(workbook, worksheet, 'Planilha');
+
+        const excelBuffer = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
+        const blob = new Blob([excelBuffer], { type: 'application/vnd.ms-excel' });
+        const url = window.URL.createObjectURL(blob);
+
+        const downloadLink = document.createElement('a');
+        downloadLink.href = url;
+        downloadLink.download = 'Registro-Tokens.xlsx';
+        downloadLink.innerHTML = 'Baixar Planilha'; // adicione o texto do link aqui
+
+        // Adicione o link em um elemento HTML, como um botão ou um link
+        const resultado = document.querySelector('.resultado');
+        resultado.appendChild(downloadLink);
+
 
         resultado.innerHTML += `<p>Nome: ${nome.value} ///  Telefone: ${telefone.value} /// Pedido: ${pedido.value} Token: ${token} </p></br>`
-        nome.value='';
-        telefone.value='';
-        pedido.value='';
-        
+        nome.value = '';
+        telefone.value = '';
+        pedido.value = '';
+
     }
-    
+
     form.addEventListener('submit', recebeEventoForm)
+
+
 }
 meuEscopo()
